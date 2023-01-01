8733a928e4c1a7a8c320d20b2b5264f2bf673a09
hama <>
2023-01-01 15:36:36 +0900

    課題1

--- src/ExeTest.java
+++ src/ExeTest.java
@@ -1,6 +1,12 @@
-
 public class ExeTest {
 	public static void main(String[] args) {
-		
+		String inputString = GameUtils.getInputString();
+		int inputInt = GameUtils.getInputInt();
+		int randomInt = GameUtils.getRandomInt(5);
+		boolean result = GameUtils.checkPattern("test");
+		System.out.println(inputString);
+		System.out.println(inputInt);
+		System.out.println(randomInt);
+		System.out.println(result);
 	}
 }
---
+++ src/GameUtils.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+import java.util.regex.Pattern;
+
+class GameUtils {
+	private static final String REGEX_ALPHABET = "^[A-Za-z]{4,12}$";
+	
+	private GameUtils(){}
+	
+	public static String getInputString() {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String inputString = null;
+		try {
+			inputString = br.readLine();			
+		} catch(IOException e) {
+			System.out.println("Input error...Please retype!");
+			getInputString();
+		}
+		return inputString;
+	}
+	
+	public static int getInputInt() {
+		int inputInt = 0;
+		try {
+			inputInt = Integer.parseInt(getInputString());
+		} catch(NumberFormatException e) {
+			System.out.println("Please enter an Integer!");
+			getInputInt();
+		}
+			
+		return inputInt;
+	}
+	
+	public static int getRandomInt(int maxValue) {
+		Random random = new Random();
+		return random.nextInt(maxValue);
+		
+	}
+	
+	public static boolean checkPattern(String targetStr) {
+		if (targetStr == null || targetStr.isEmpty()) {
+			return false;
+		} 
+		return Pattern.matches(REGEX_ALPHABET, targetStr);
+
+	}
+}
