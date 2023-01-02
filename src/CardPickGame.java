dce8d32cb9edb9d580f318956261daf82c6da35b
hama <hama@192.168.100.115>
2023-01-02 18:33:47 +0900

    課題4　自分の回答

--- src/CardPickGame.java
+++ src/CardPickGame.java
@@ -9,44 +9,6 @@ public class CardPickGame {
 	}
 	
 	public void execute() {
-<<<<<<< Updated upstream
-=======
-		int userNumSum = getCard();
-		System.out.println(judgeCard(userNumSum));
-	}
-	
-	private int getCard() {
-		List<List<Integer>> setDeck = new ArrayList<List<Integer>>();
-		List<Integer> onePair = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-		for (int i = 0; i < this.deckSetCount; i++) {
-			setDeck.add(i, onePair);
-		}
-		
-		int randNumA1;
-		int randNumA2;
-		int randNumB1;
-		int randNumB2;
-		
-		while (true) {
-			randNumA1 = GameUtils.getRandomInt(2);
-			randNumA2 = GameUtils.getRandomInt(10);
-			randNumB1 = GameUtils.getRandomInt(2);
-			randNumB2 = GameUtils.getRandomInt(10);
-			
-			if (!(randNumA1 == randNumB1 && randNumA2 == randNumB2)) {
-				break;
-			}
-		}
-		
-		
-		int cardA = setDeck.get(randNumA1).get(randNumA2);
-		int cardB = setDeck.get(randNumB1).get(randNumB2);
-		int userNumSum = cardA + cardB;
-		
-		
-		System.out.println("Cards drawn are " + cardA + " and " + cardB + ", total is " + userNumSum);
->>>>>>> Stashed changes
-		
 	}
 	
 	private boolean judgeCard(int getCardResult) {
